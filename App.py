d9d19e470d1c00a6f3f2dc67d5cd7d2b5df7736d
Chirag Hira <100068374+ChiragHira@users.noreply.github.com>
2024-04-09 23:55:49 +0530

    Add files via upload

--- App.py
+++ App.py
@@ -1,13 +1,29 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import mysql.connector
 
 app = Flask(__name__)
+mydb = mysql.connector.connect(
+    host="localhost",
+    user = "root",
+    password = "Chirag@29"
+)
 
 ## Home Route
 @app.route("/")
 def helloWorld():
     return render_template("index.html")
+
+@app.route("/store")
+def storeInfo():
+    if request.method=='POST':
+        name = request.form('Name')
+        email = request.form('Email')
+        branch=request.form('Branch')
+        collage=request.form('Collage')
+        
+    
     
     
 if __name__=="__main__":
